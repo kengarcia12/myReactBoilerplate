a03f6fccb667308008033bc662ac1fcf4faab8b4
Ray Abanid <ray.abanid@gmail.com>
2017-12-08 14:57:04 +0800

    Added components

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import Header from './common/header';
+import Footer from './common/footer';
+
+
 
 
 export default class Layout extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            title: "Welcome",
+        }
+    }
+
+    changeTitle(title){
+        this.setState({ title })
+    }
+
     render(){
         return (
-            <h1> Layout Section </h1>
+            <div>
+                <Header title={this.state.title} changeTitle={this.changeTitle.bind(this)} />
+                <Footer />
+            </div>
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 /*
     ./client/index.js
     which is the webpack entry file
+    //yarn start
 */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
+import Layout from './components/layout/index';
 
 const app = document.getElementById('root');
 
-ReactDOM.render(<App />, app );
+ReactDOM.render(<Layout />, app );
